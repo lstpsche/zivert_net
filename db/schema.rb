c8cc822e04c141d9aa0a8d23e0b1c784796cadfd
Nikita Silivonchik <lstpsche@gmail.com>
2020-05-14 00:22:06 +0300

    Create GeoPoint migration and model refs: #2

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_13_210837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "geo_points", force: :cascade do |t|
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
