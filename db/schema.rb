123642f56f3f7efd6e93a0fc41752ae7bd075673
Nikita Silivonchik <lstpsche@gmail.com>
2020-05-14 17:48:57 +0300

    Create User model and add migration for users refs: #ZN-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_210837) do
+ActiveRecord::Schema.define(version: 2020_05_14_100239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_05_13_210837) do
   create_table "geo_points", force: :cascade do |t|
     t.integer "width", default: 0, null: false
     t.integer "height", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
