d72fcac6728e3752fec6f8279c120aafc35b7e29
Nikita Silivonchik <lstpsche@gmail.com>
2021-02-18 01:47:08 +0300

    Fix geo_point update store action, fix geo_point rad_value recalculation call logic, update tests refs: ZN-54

--- app/jobs/measurements/base_job.rb
+++ app/jobs/measurements/base_job.rb
@@ -5,6 +5,8 @@ module Measurements
     queue_as :urgent
 
     def perform(action, measurement)
+      MeasurementsCore::GeoPointValueCalculationService.new(measurement.geo_point).calculate
+
       ActionCable.server.broadcast('measurements_channel', action: action, measurement: measurement.json)
     end
   end
