a9d3fbd3e203db11907caf3392aece047ddb3f0b
Nikita Silivonchik <lstpsche@gmail.com>
2021-02-20 17:35:36 +0300

    Move jobs calling methods to base application_record class ref: ZN-59

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,24 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def json
+    "#{class_name}Serializer".safe_constantize&.new(self)&.serializable_hash
+  end
+
+  def broadcast_creation
+    "#{class_name.pluralize}::CreationJob".safe_constantize&.perform_now(self)
+  end
+
+  def broadcast_updation
+    "#{class_name.pluralize}::UpdationJob".safe_constantize&.perform_now(self)
+  end
+
+  def broadcast_deletion
+    "#{class_name.pluralize}::DeletionJob".safe_constantize&.perform_now(self)
+  end
+
+  def class_name
+    self.class.name
+  end
 end
