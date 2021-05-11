00984acef37943c9d61f2a4bdc6ea343481e674d
Nikita Silivonchik <lstpsche@gmail.com>
2021-05-11 22:32:12 +0300

    Measurement creation: add input append for rad value field refs: ZN-73

--- app/frontend/javascript/helpers/generate_marker_class_name.js
+++ app/frontend/javascript/helpers/generate_marker_class_name.js
@@ -2,13 +2,17 @@ const valueThresholds = {
   20: "default",
   50: "warning",
   100: "danger",
-  default: "critical"
+  overhead: "critical",
+  default: "default"
 }
 
 function generateMarkerClassName (value) {
+  if (value === "" || value === undefined)
+    return valueThresholds["default"];
+
   const threshold = Object.keys(valueThresholds).find(threshold => value < threshold);
 
-  return valueThresholds[threshold] || valueThresholds["default"];
+  return valueThresholds[threshold] || valueThresholds["overhead"];
 }
 
 export default generateMarkerClassName;
